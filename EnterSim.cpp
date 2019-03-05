0871f0edd3be9042d86d760fc01502e3d8bbf4a7
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-03-05 07:01:44 +0000

    Added comments to EnterSim.

--- EnterSim.cpp
+++ EnterSim.cpp
@@ -1,6 +1,14 @@
+/*
+  Makenzie De Armas
+  ID: 2278709
+  dearm102@mail.chapman.edu
+  CPSC 350-01
+  Assignment 1: C++ Review
+  Purpose: This is contains the out-of-class member function for EnterSim.
+*/
 #include "EnterSim.h"
 
 void EnterSim::next()
 {
-  pressEnterToContinue();
+  pressEnterToContinue(); //now, when next() is invoked, the user must press [ENTER] to continue
 }
--- EnterSim.h
+++ EnterSim.h
@@ -1,3 +1,11 @@
+/*
+  Makenzie De Armas
+  ID: 2278709
+  dearm102@mail.chapman.edu
+  CPSC 350-01
+  Assignment 1: C++ Review
+  Purpose: This is the header file for the EnterSim class (child of Simulation). It overrides the next() function so that the simulation will only proceed when the user presses [ENTER].
+*/
 #ifndef ENTER_SIM_H_
 #define ENTER_SIM_H_
 
@@ -5,8 +13,8 @@
 
 class EnterSim : public Simulation {
 public:
-  EnterSim():Simulation(){};
-  virtual void next();
+  EnterSim():Simulation(){}; //default constructor
+  virtual void next(); //overwritten next() function to employ pressEnterToContinue()
 };
 
 #endif
